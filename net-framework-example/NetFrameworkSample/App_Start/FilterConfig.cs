927b65116c8bc51b3749cec75e00d6ec5ade3beb
Chris Ayers <clayers@gmail.com>
2022-01-24 21:06:22 +0000

    Added framework example

---
+++ net-framework-example/NetFrameworkSample/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NetFrameworkSample
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ net-framework-example/NetFrameworkSample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NetFrameworkSample.MvcApplication" Language="C#" %>
