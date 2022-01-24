0662816e0a1f7ecf59361e7e5f9150b7921a7730
Chris Ayers <clayers@gmail.com>
2022-01-24 03:16:54 +0000

    Added json configuration

---
+++ 01-console-configuration-json/Program.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .Build();
+
+var name = "World";
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"{configuration["greeting"]}, {name}!");
+Console.WriteLine($"Configuration: {configuration["environment"]}");
