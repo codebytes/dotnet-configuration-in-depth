b61eab74a61d0687b4109895266dc1a4cc024b28
Chris Ayers <clayers@gmail.com>
2022-01-24 23:58:04 +0000

    Added sections and bindings

---
+++ 10-section-binding/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>(optional: true)
+    .AddCommandLine(args)
+    .Build();
+
+
+Console.WriteLine($"markdownStyle: {configuration["markdownStyle"]}");
+
+var pdfOptions = new FileOptions();
+configuration.GetSection("pdf")
+    .Bind(pdfOptions);
+WriteOptions(pdfOptions);
+
+var docOptions = configuration.GetSection("doc")
+    .Get<FileOptions>();
+WriteOptions(docOptions);
+
+var htmlOptions = configuration.GetSection("html")
+    .Get<FileOptions>();
+WriteOptions(htmlOptions);
+
+
+void WriteOptions(FileOptions options)
+{
+    Console.WriteLine($"{options.FileExtension} fileExtension: {options.FileExtension}");
+    Console.WriteLine($"{options.FileExtension} OutputDir: {options.OutputDir}");
+    Console.WriteLine($"{options.FileExtension} TemplateFile: {options.TemplateFile}");
+}
+
+
+public class FileOptions
+{
+    public string FileExtension { get; set; } ="";
+    public string OutputDir { get; set; } ="";
+    public string TemplateFile { get; set; } ="";
+}
