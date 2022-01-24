c2e508b6ac9e40b3b63557f84f031aa31bc4acfe
Chris Ayers <clayers@gmail.com>
2022-01-24 03:17:43 +0000

    added Config sections

---
+++ 05-console-config-sections/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>(optional: true)
+    .AddCommandLine(args)
+    .Build();
+
+var name = args.Any() ? args[0] : "World";
+// See https://aka.ms/new-console-template for more information
+AnsiConsole.MarkupLine($"[{configuration["greeting:color"]}]{configuration["greeting:message"]}, {name}![/]");
+Console.WriteLine($"Configuration: {configuration["environment"]}");
