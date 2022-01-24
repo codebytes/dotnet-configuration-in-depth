5310bbe58b61ee8d590c649b34228af197f95c25
Chris Ayers <clayers@gmail.com>
2022-01-24 03:17:31 +0000

    added  user-secrets

---
+++ 04-console-configuration-user-secrets/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using Azure.Identity;
+using Azure.Security.KeyVault;
+using Azure.Security.KeyVault.Secrets;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>()
+    .AddCommandLine(args)
+    .Build();
+
+var kvUri = "https://" + configuration["key_vault_name"] + ".vault.azure.net";
+
+var client = new SecretClient(new Uri(kvUri), 
+    new ClientSecretCredential(
+        configuration["AZURE_TENANT_ID"], 
+        configuration["AZURE_CLIENT_ID"], 
+        configuration["AZURE_CLIENT_SECRET"]));
+
+var key = await client.GetSecretAsync("myKey");
+Console.WriteLine($"Mykey: {key.Value.Value}");
+
+var name = args.Any() ? args[0] : "World";
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"{configuration["greeting"]}, {name}.");
+Console.WriteLine($"Environment: {configuration["environment"]}");
