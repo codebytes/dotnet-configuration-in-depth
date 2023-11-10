8cbb204e87bf280b87528b22da774bb6f665d69f
Chris Ayers <clayers@gmail.com>
2023-11-10 16:31:29 +0000

    restructured demos.

---
+++ 09-hosted-console-template/Program.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.Extensions.Hosting;
+
+using IHost host = Host.CreateApplicationBuilder(args).Build();
+
+// Application code should start here.
+Console.WriteLine("Hello, World!");
+await host.RunAsync();
