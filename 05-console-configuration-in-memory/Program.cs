ead2fd8fe1e6b1a952c5d084c007e106cb4747fd
Chris Ayers <clayers@gmail.com>
2024-05-06 21:42:19 +0000

    chore: Update dependencies and remove unused files

---
+++ 05-console-configuration-in-memory/Program.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+
+var configuration = new ConfigurationBuilder()
+    .AddInMemoryCollection(new Dictionary<string, string?>()
+    {
+        ["greeting"] = "Hello",
+        ["environment"] = "dev"
+    })
+    .Build();
+
+Console.WriteLine(configuration["SomeKey"]);
+var name = "Chris";
+// Outputs:
+//   SomeValue
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine($"{configuration["greeting"]}, {name}!");
+Console.WriteLine($"Configuration: {configuration["environment"]}");
---
+++ 06-console-config-sections/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
+
+IConfigurationRoot configuration = new ConfigurationBuilder()
+    .AddJsonFile("config.json")
+    .AddEnvironmentVariables()
+    .AddUserSecrets<Program>(optional: true)
+    .AddCommandLine(args)
+    .Build();
+
+var name = "World";
+
+//dotnet run --greeting:color="blue"
+
+AnsiConsole.MarkupLine($"[{configuration["greeting:color"]}]{configuration["greeting:message"]}, {name}![/]");
+Console.WriteLine($"Configuration: {configuration["environment"]}");
